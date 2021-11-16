ae42ca02e2deeffae9fc37afa41c3c8853b366bc
aidantai <aidantai100@gmail.com>
2021-11-15 23:18:57 -0500

    matching
    comp assignment #2

--- assignment2/match.py
+++ assignment2/match.py
@@ -1,6 +1,14 @@
 import numpy as np
 from typing import List, Tuple
 
+class User:
+    def __init__(self, index, gender, pref, remaining):
+        self.index = index
+        self.gender = gender
+        self.pref = pref
+        self.remaining = remaining
+        self.match = self
+
 def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List[Tuple]:
     """
     TODO: Implement Gale-Shapley stable matching!
@@ -21,7 +29,80 @@ def run_matching(scores: List[List], gender_id: List, gender_pref: List) -> List
             - What data structure can you use to take advantage of this fact when forming your matches?
         - This is by no means an exhaustive list, feel free to reach out to us for more help!
     """
-    matches = [()]
+
+    total = len(gender_id)
+
+    users = []
+    for u in range(total):
+        pref = gender_pref[u]
+        gender = gender_id[u]
+        remaining = scores[u]
+        new_user = User(u, gender, pref, remaining)
+        users.append(new_user)
+
+    proposers = []
+    for i in range(int(total/2)):
+        u = users[i]
+        new_remaining = []
+        for j in range(int(total/2), total):
+            new_remaining.append((users[j], scores[i][j]))
+        u.remaining = new_remaining
+        proposers.append(u)
+
+    receivers = []
+    for i in range(int(total/2), total):
+        u = users[i]
+        new_remaining = []
+        for j in range(int(total/2)):
+            new_remaining.append((users[j], scores[i][j]))
+        u.remaining = new_remaining
+        receivers.append(u)
+
+    for p in proposers:
+        for r in receivers:
+            if ((p.pref != "Men" and r.gender == "Male")
+                    or (p.pref != "Women" and r.gender == "Female")
+                    or (r.pref != "Men" and p.gender == "Male")
+                    or (r.pref != "Women" and p.gender == "Female")):
+                # p.remaining[r.u] = (r, 0.0)
+                # r.remaining[p.u] = (p, 0.0)
+                p.remaining = [(x, -1.0) if x == r else (x, y) for (x, y) in p.remaining]
+                r.remaining = [(x, -1.0) if x == p else (x, y) for (x, y) in r.remaining]
+
+    for u in users:
+        # create remaining list sorted by priority with user index saved
+        u.remaining.sort(key=lambda i: i[1], reverse=True)
+        print(u.index)
+        for x, y in u.remaining:
+            print(x.index, y)
+        print()
+
+    matches = []
+    while proposers != []:
+        p = proposers[0]  # pick first proposer in free list
+        if p.remaining != []:
+            r, rscore = p.remaining.pop(0)
+            # r = max(p.remaining, key=lambda x: x[1])[0]  # find max score remaining
+            # p.remaining.pop(r.u)  # remove r from list
+            pscore = [y for (x, y) in r.remaining if x == p]
+            if r in receivers:  # if unmatched
+                p.match = r
+                r.match = p
+                proposers.remove(p)
+                receivers.remove(r)
+            elif pscore > [y for (x, y) in r.remaining if x == r.match]:
+                proposers.append(r.match)
+                p.match = r
+                r.match = p
+                proposers.remove(p)
+
+    for i in range(int(total)):
+        p = users[i]
+        matches.append((p, p.match))
+
+    for x, y in matches:
+        print(x.index, y.index)
+
     return matches
 
 if __name__ == "__main__":
